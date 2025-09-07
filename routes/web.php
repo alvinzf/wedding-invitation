13448b80cd20e127c09e39a7f0c7c45e944252da
Alvin Zulham <alvinzulhamm@gmail.com>
2025-09-07 23:54:50 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ Route::post('/home', [HomeController::class, 'store']);
 Route::get('/admin/guests/{record}', function ($record) {
     return redirect()->route('filament.admin.resources.guests.view', $record);
 })->name('filament.admin.resources.guests.view');
-Route::get('/attendance', [GuestAttendanceController::class, 'index'])->name('attendance');
-Route::get('/attendance/table', [GuestAttendanceController::class, 'table'])->name('attendance.table');
-Route::get('/attendance/detail', [GuestAttendanceController::class, 'detail'])->name('attendance.detail');
-Route::post('/attendance/submit', [GuestAttendanceController::class, 'submit'])->name('attendance.submit');
+// Route::get('/attendance', [GuestAttendanceController::class, 'index'])->name('attendance');
+// Route::get('/attendance/table', [GuestAttendanceController::class, 'table'])->name('attendance.table');
+// Route::get('/attendance/detail', [GuestAttendanceController::class, 'detail'])->name('attendance.detail');
+// Route::post('/attendance/submit', [GuestAttendanceController::class, 'submit'])->name('attendance.submit');
 Route::post('/guest-confirmation', [GuestAttendanceController::class, 'guestConfirmation'])->name('guest-confirmation');
 Route::get('/guest-wishes', [GuestAttendanceController::class, 'guestWishes'])->name('guest-wishes');
 Route::post('/guest-send-wish', [GuestAttendanceController::class, 'sendWish'])->name('send-wish');
